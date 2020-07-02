843b428b9cfb6f32701b049f049dd92d3705fe73
Gustave Anthony Dzoumedor <dzoumedor13@gmail.com>
2020-07-02 04:39:25 +0000

    Added Modal & Contacted it to firebase

--- index.js
+++ index.js
@@ -21,49 +21,102 @@ function Budget(e) {
 }
 
 //Fetching data from Json/Api
-function images() {
-  var API_KEY = "17201394-2c25b09720620fe3bab7a0f30";
-  var URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(school.value);
+function hostels() {
+  Budget();
 
-  fetch(URL)
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.hits == 0) {
-        document.querySelector(".card-group").innerHTML = "";
-        document.querySelector(".card-group").innerHTML += `
-          <div class="alert alert-danger mt-5 offset-sm-2 col-sm-12 col-md-12 mr-lg-5 offset-md-7 offset-lg-11" role="alert">
-    <h4 class="text-center"> No search result found </h4>`;
-        document.querySelector("footer").innerHTML = "";
-      } else {
+  var firebaseConfig = {
+    apiKey: "#",
+    authDomain: "#",
+    databaseURL: "#",
+    projectId: "#",
+    storageBucket: "#",
+    messagingSenderId: "#",
+    appId: "#",
+  };
+  // Initialize Firebase
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+  var db = firebase.database();
+
+  var university = db.ref();
+
+  university.on("value", (snap) => {
+    let price;
+    let images;
+    snap.forEach((data) => {
+      var dataKey = data.key;
+      var dataInfo = data.val();
+
+      //Comparing user input to database key to get the right university
+      if (dataKey == school.value) {
         document.querySelector("#perfect").style.marginTop = "50px";
         document.querySelector(".card-group").innerHTML = "";
-        data.hits.forEach((image) => {
+        dataInfo[school.value].map((info) => {
+          //Looping through images array
+          // info.images.forEach((image) => {
+          //   images = image;
+          // });
+          // console.log(images);
+
+          //comparing user input to database figures
+          if (info.budget <= 300 && budgetValue < 300) {
+            price = info.budget;
+          } else if (info.budget <= 600 && budgetValue <= 600) {
+            price = info.budget;
+          } else if (info.budget >= 599 && budgetValue > 599) {
+            price = info.budget;
+          }
           document.querySelector(".card-group").innerHTML += `
-        <div class="col-12 col-md-6 col-lg-4 offset-sm-2 offset-md-0 p-3">
-        <div class="card" style="width: 18rem;">
-          <img src="${image.userImageURL}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <p class="card-text">Name: <span class="font-weight-bolder"> ${image.tags} </span>  <br> 
-        <abbr title="Telephone">Tel: <a href="tel:+2562501258">(256)2501258</a></abbr> <br>
-            <abbr title="Mail">Email: <a href="mailto:example@mail.com">example@mail.com</a></abbr>      
-            </p>
-          </div>
-        </div>
-        </div>`;
+          <div class="col-12 col-md-6 col-lg-4 offset-sm-2 offset-md-0 p-3">
+            <div class="card" style="width: 20rem;">
+            <a href="#" data-toggle="modal" data-target="#staticBackdrop"> <img src="${info.previewImage}" class="card-img-top" alt="..."></a> 
+            <div class="card-body">
+                <p class="card-text" >Name: <span class="font-weight-bolder"> ${info.hostelName} </span>
+                <span class="price float-right p-2 text-white rounded font-weight-bolder" style="background:#252E56 ;">GHC ${price}</span>
+                   <br> ${info.location} . <abbr title="Telephone"><a href="tel:+2562501258">${info.contact}</a></abbr> <br>
+                </p>
+                <p>${info.overview}</p>
+              </div>
+            </div>
+            </div>`;
+
+          document.querySelector(".carousel").innerHTML = `
+            <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="${info.images}" class="d-block w-100" alt="...">
+            </div>
+            </div>
+            `;
+
           document.querySelector("footer").innerHTML = `
-        <h5><a class="git" href="https://github.com/stavio12" target="_blank" ><i class="fa fa-github text-white" aria-hidden="true"></i> Kobla Stavio</a></h5>
-        <h6 >@2020 All rights reserved</h6>
-      `;
+            <h5><a class="git" href="https://github.com/stavio12" target="_blank" ><i class="fa fa-github text-white" aria-hidden="true"></i> Kobla Stavio</a></h5>
+            <h6 >@2020 All rights reserved</h6>
+          `;
         });
+      } else {
+        document.querySelector(".card-group").innerHTML = "";
+        document.querySelector(".card-group").innerHTML += `
+              <div class="alert alert-danger mt-5 offset-sm-2 col-sm-12 col-md-12 mr-lg-5 offset-md-7 offset-lg-11" role="alert">
+        <h4 class="text-center"> No search result found </h4>`;
+        document.querySelector("footer").innerHTML = "";
       }
-    })
-    .catch((error) => console.error(error));
+    });
+  }),
+    (error) => {
+      console.log("Error", error);
+    };
 }
+
+// function hosteldata() {
 
 function search(e) {
   e.preventDefault();
+  // Budget();
 
-  images();
+  hostels();
 
-  form.reset();
+  // console.log(budgetValue);
+
+  // form.reset();
 }
