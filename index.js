3b3bd109fa74618cce44ae85be6ed2c3ef56d795
Gustave Anthony Dzoumedor <dzoumedor13@gmail.com>
2020-07-09 16:09:51 +0000

    files cleanup

--- index.js
+++ index.js
@@ -51,56 +51,55 @@ function hostels() {
         document.querySelector("#perfect").style.marginTop = "50px";
         document.querySelector(".card-group").innerHTML = "";
         dataInfo[school.value].map((info) => {
-          info.images.forEach((image) => {
-            images = image;
-          });
-
           // querying  through hostels depending on prices
 
           if (info.budget <= 300 && budgetValue == 299) {
             price = info.budget;
-            console.log("high", price);
-            console.log("------------");
 
             if (typeof price !== "undefined" && price.length) {
-              console.log(price.length);
-
               document.querySelector(".card-group").innerHTML += `
-                        <div class="col-12 col-md-6 col-lg-4 offset-sm-2 offset-md-0 p-3">
-                          <div class="card" style="width: 20rem;">
-                          <a href="#" data-toggle="modal" data-target="#staticBackdrop"> <img src="${info.previewImage}" class="card-img-top" alt="..."></a> 
-                          <div class="card-body">
-                              <p class="card-text" >Name: <span class="font-weight-bolder"> ${info.hostelName} </span>
-                              <span class="price float-right p-2 text-white rounded font-weight-bolder" style="background:#252E56 ;">GHC ${price}</span>
-                                 <br> ${info.location} . <abbr title="Telephone"><a href="tel:+2562501258">${info.contact}</a></abbr> <br>
-                              </p>
-                              <p>${info.overview}</p>
-                            </div>
-                          </div>
-                          </div>`;
+              <div class="col-12 col-md-6 col-lg-4 offset-sm-2 offset-md-0 p-3">
+                <div class="card" style="width: 20rem;">
+                <a href="#" data-toggle="modal" data-target="#staticBackdrop"> <img src="${info.previewImage}" class="card-img-top" alt="..."></a> 
+                <div class="card-body">
+                    <p class="card-text" >Name: <span class="font-weight-bolder"> ${info.hostelName} </span>
+                    <span class="price float-right p-2 text-white rounded font-weight-bolder" style="background:#252E56 ;">GHC ${price}</span>
+                       <br> ${info.location} . <abbr title="Telephone"><a href="tel:+2562501258">${info.contact}</a></abbr> <br>
+                    </p>
+                    <p>${info.overview}</p>
+                  </div>
+                </div>
+                </div>`;
+
               document.querySelector(".carousel").innerHTML = `
-                          <div class="carousel-inner">
-                         <div class="carousel-item active">
-                          <img src="${images}" class="d-block w-100" alt="...">
-                          </div>
-                          <div class="carousel-item">
-                          <img src="${images}" class="d-block w-100" alt="...">
-                          </div>
-                          </div>
-                          `;
+                <div class="carousel-inner">
+               <div class="carousel-item active">
+                <img src="${info.images}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="${info.images}" class="d-block w-100" alt="...">
+                </div>
+  
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+                </div>
+                `;
 
               document.querySelector("footer").innerHTML = `
-                          <h5><a class="git" href="https://github.com/stavio12" target="_blank" ><i class="fa fa-github text-white" aria-hidden="true"></i> Kobla Stavio</a></h5>
-                          <h6 >@2020 All rights reserved</h6>
-                        `;
+                <h5><a class="git" href="https://github.com/stavio12" target="_blank" ><i class="fa fa-github text-white" aria-hidden="true"></i> Kobla Stavio</a></h5>
+                <h6 >@2020 All rights reserved</h6>
+              `;
             }
           } else if (info.budget >= 300 && info.budget <= 600 && budgetValue == 300) {
             price = info.budget;
-            console.log("medium", price);
-            console.log("------------");
-            if (typeof price !== "undefined" && price.length) {
-              console.log(price.length);
 
+            if (typeof price !== "undefined" && price.length) {
               document.querySelector(".card-group").innerHTML += `
                         <div class="col-12 col-md-6 col-lg-4 offset-sm-2 offset-md-0 p-3">
                           <div class="card" style="width: 20rem;">
@@ -118,11 +117,20 @@ function hostels() {
               document.querySelector(".carousel").innerHTML = `
                           <div class="carousel-inner">
                          <div class="carousel-item active">
-                          <img src="${images}" class="d-block w-100" alt="...">
+                          <img src="${info.images}" class="d-block w-100" alt="...">
                           </div>
                           <div class="carousel-item">
-                          <img src="${images}" class="d-block w-100" alt="...">
+                          <img src="${info.images}" class="d-block w-100" alt="...">
                           </div>
+            
+                          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
                           </div>
                           `;
 
@@ -133,41 +141,46 @@ function hostels() {
             }
           } else if (info.budget >= 600 && budgetValue == 600) {
             price = info.budget;
-            console.log("low", price);
-            console.log("------------");
 
             if (typeof price !== "undefined" && price.length) {
-              console.log(price.length);
-
               document.querySelector(".card-group").innerHTML += `
-                                    <div class="col-12 col-md-6 col-lg-4 offset-sm-2 offset-md-0 p-3">
-                                      <div class="card" style="width: 20rem;">
-                                      <a href="#" data-toggle="modal" data-target="#staticBackdrop"> <img src="${info.previewImage}" class="card-img-top" alt="..."></a> 
-                                      <div class="card-body">
-                                          <p class="card-text" >Name: <span class="font-weight-bolder"> ${info.hostelName} </span>
-                                          <span class="price float-right p-2 text-white rounded font-weight-bolder" style="background:#252E56 ;">GHC ${price}</span>
-                                             <br> ${info.location} . <abbr title="Telephone"><a href="tel:+2562501258">${info.contact}</a></abbr> <br>
-                                          </p>
-                                          <p>${info.overview}</p>
-                                        </div>
-                                      </div>
-                                      </div>`;
+              <div class="col-12 col-md-6 col-lg-4 offset-sm-2 offset-md-0 p-3">
+                <div class="card" style="width: 20rem;">
+                <a href="#" data-toggle="modal" data-target="#staticBackdrop"> <img src="${info.previewImage}" class="card-img-top" alt="..."></a> 
+                <div class="card-body">
+                    <p class="card-text" >Name: <span class="font-weight-bolder"> ${info.hostelName} </span>
+                    <span class="price float-right p-2 text-white rounded font-weight-bolder" style="background:#252E56 ;">GHC ${price}</span>
+                       <br> ${info.location} . <abbr title="Telephone"><a href="tel:+2562501258">${info.contact}</a></abbr> <br>
+                    </p>
+                    <p>${info.overview}</p>
+                  </div>
+                </div>
+                </div>`;
 
               document.querySelector(".carousel").innerHTML = `
-                                      <div class="carousel-inner">
-                                      <div class="carousel-item active">
-                                      <img src="${images}" class="d-block w-100" alt="...">
-                                      </div>
-                                      <div class="carousel-item">
-                                      <img src="${images}" class="d-block w-100" alt="...">
-                                      </div>
-                                      </div>
-                                      `;
+                <div class="carousel-inner">
+               <div class="carousel-item active">
+                <img src="${info.images}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="${info.images}" class="d-block w-100" alt="...">
+                </div>
+  
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+                </div>
+                `;
 
               document.querySelector("footer").innerHTML = `
-                                      <h5><a class="git" href="https://github.com/stavio12" target="_blank" ><i class="fa fa-github text-white" aria-hidden="true"></i> Kobla Stavio</a></h5>
-                                      <h6 >@2020 All rights reserved</h6>
-                                    `;
+                <h5><a class="git" href="https://github.com/stavio12" target="_blank" ><i class="fa fa-github text-white" aria-hidden="true"></i> Kobla Stavio</a></h5>
+                <h6 >@2020 All rights reserved</h6>
+              `;
             }
           }
         });
